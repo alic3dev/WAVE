bbdc87dcdd34d99fcf20a50dcf85f510adf371e4
alic3 <alic3dev@gmail.com>
2025-01-02 16:31:32 -0500

    `integer` -> `value`

--- sources/to_bytes.c
+++ sources/to_bytes.c
@@ -3,18 +3,18 @@
 
 #include "to_bytes.h"
 
-char* to_bytes_int(int integer, size_t length, unsigned char little_endian) {
+char* to_bytes_int(int value, size_t length, unsigned char little_endian) {
   char *bytes;
   bytes = malloc(length);
 
   size_t index = little_endian == 1 ? 0 : length - 1;
   do {
-    if (integer > 255) {
+    if (value > 255) {
       bytes[index] = (char) 255;
-      integer -= 255;
+      value -= 255;
     } else {
-      bytes[index] = (char) integer;
-      integer = 0;
+      bytes[index] = (char) value;
+      value = 0;
     }
 
     if (little_endian == 1) {
@@ -22,7 +22,7 @@ char* to_bytes_int(int integer, size_t length, unsigned char little_endian) {
     } else {
       index--;
     }
-  } while (integer > 0 && index < length && index > 0);
+  } while (value > 0 && index < length && index > 0);
 
   return bytes;
 }
